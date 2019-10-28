0aad8f820f21c09f3cc3c2594f30d55c37ab5620
Дмитрий <rabolan@mail.ru>
2019-10-28 18:11:52 +0300

    Рабочая версия печати баркода

--- Forms/Qkod.cs
+++ Forms/Qkod.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -60,5 +61,37 @@ namespace SendingChecksOFD.Forms
         {
             pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
         }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            //PrintDocument pd = new PrintDocument();
+            //pd.PrintPage += PrintPage;
+            //pd.Print();
+
+            System.Drawing.Printing.PrintDocument printDocument = new System.Drawing.Printing.PrintDocument();
+            printDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(printDocument_PrintPage);
+
+            PrintDialog printDlg = new PrintDialog();
+
+            if (printDlg.ShowDialog() == DialogResult.OK)
+            {
+                printDocument.PrinterSettings = printDlg.PrinterSettings;
+                printDocument.Print();
+               
+            }
+        }
+
+        void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            //   Paint(e.Graphics);// Выводит на екран линии и текст (по сути мой рисунок)
+            e.Graphics.DrawImage(pictureBox.Image, 0, 0);
+        }
+        //private void PrintPage(object o, PrintPageEventArgs e)
+        //{
+        //    System.Drawing.Image img = System.Drawing.Image.FromFile(pictureBox.Image.);
+        //    Point loc = new Point(100, 100);
+        //    e.Graphics.DrawImage(img, loc);
+        //}
+
     }
 }
